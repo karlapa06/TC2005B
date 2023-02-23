40f4b8ee711fb1f41ed5b8c5805f55fb8a7ded46
KARLA PADILLA <karlapadilla@MacBook-Air-de-KARLA-4.local>
2023-02-22 21:53:30 -0600

    Actualizar Lab6

---
+++ Lab 6/js6.js
@@ -0,0 +1,17 @@
+function validarPasswords(){
+    var p1 = document.getElementById("password").value;
+    var p2 = document.getElementById("password2").value;
+
+    if (p1 == p2) {
+        alert("Las passwords deben de coincidir");
+        return false;
+      } else {
+        alert("Todo esta correcto");
+        return true; 
+      }
+
+}
+document.getElementById("enviar").addEventListener('click',Valid)
+
+
+
