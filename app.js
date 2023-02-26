452fbd28e8289cadc5a4716570d0859c5608373c
KARLA PADILLA <A01705331@tec.mx>
2023-02-25 21:51:52 -0600

    Agregar Laboratorio

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// consit filesystems = required ('fs'); **para acceder y más a los archivos
+// filesystems.writeFileSync('hola.txt','Hola desde node');
+
+// const http = require('http');
+// const server = http.createServer();
+
+// server.listen (3000) ** para crear un servidor con un puerto 
+
